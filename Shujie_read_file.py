5450bc46805767ddaf518d0672eab0709c1efa41
Shujie <s.j.zhu@outlook.com>
2024-02-12 01:42:29 +0000

    Fixed the code

--- Shujie_read_file.py
+++ Shujie_read_file.py
@@ -1,33 +1,31 @@
 # Q2 2 Use open and read method to display the context of the txt file
 
-pelican_poem = open('spelican.txt', 'r')
-print(pelican_poem.read())
+pelican_poem = open('spelican.txt', 'r').read()
+print(pelican_poem)
 # 'r' to read the file
 
 # 3 & 4 What data type is the output?
 
-print(f"The return data type is {type(pelican_poem.read())}")
+print(f"The return data type is {type(pelican_poem)}")
 # The data printed is a string
 
 # 5 Write some code to turn the txt file into a list and output the number of items within the list
 
-# doesn't work, check back on it-------------------------
-
-pelican_string = pelican_poem.readline()
+pelican_string = pelican_poem
 print(pelican_string)
-pelican_list = pelican_string.split()
+pelican_list = pelican_string.split('\n')
 # created a new variable to contain the poem as a list
 # by splitting the string at where the \n new line is turns it into a list
 print(pelican_list)
 print(f"The return data type is now {type(pelican_list)}")
 print(f"The length of the list is {len(pelican_list)}")
+print('\n')
 
 # 6 loop iterate over and print the contents of the file
 
-for i in range(5):
-    # read the next 5 lines
-    repeat_line = pelican_poem.readline()
+for i in range(10):
+    # range is however many times to repeat the poem
+    repeat_line = pelican_poem
     # print(line, repeat_line, end='')
-    # line 20 will also the same as line 22
     print(i, repeat_line[:-1])
-#     slicing it removes the blank linegit
+#     slicing it removes the blank line
